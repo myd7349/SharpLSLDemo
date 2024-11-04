5371893a759fb281f77b546433f1a66b8d133b75
myd7349 <myd7349@gmail.com>
2024-11-04 19:39:11 +0800

    Initial commit

---
+++ ReceiveData/Program.cs
@@ -0,0 +1,24 @@
+﻿using SharpLSL;
+
+Console.WriteLine($"64 bit? {Environment.Is64BitProcess}");
+
+// If this line of code runs successfully, it means
+// the liblsl dynamic library has been loaded successfully.
+Console.WriteLine($"liblsl version: {LSL.GetLibraryVersion()}");
+Console.WriteLine($"liblsl library info: {LSL.GetLibraryInfo()}");
+
+StreamInfo[] results = LSL.Resolve("type", "EEG");
+
+// open an inlet and print some interesting info about the stream (meta-data, etc.)
+using StreamInlet inlet = new StreamInlet(results[0]);
+Console.Write(inlet.GetStreamInfo().ToXML());
+
+// read samples
+float[] sample = new float[8];
+while (!Console.KeyAvailable)
+{
+    inlet.PullSample(sample);
+    foreach (float f in sample)
+        Console.Write("\t{0}", f);
+    Console.WriteLine();
+}
---
+++ SendData/Program.cs
@@ -0,0 +1,16 @@
+﻿using SharpLSL;
+
+Random rnd = new Random();
+
+// create stream info and outlet
+using StreamInfo info = new StreamInfo("TestCSharp", "EEG", 8, 100, ChannelFormat.Float, "sddsfsdf");
+using StreamOutlet outlet = new StreamOutlet(info);
+float[] data = new float[8];
+while (!Console.KeyAvailable)
+{
+    // generate random data and send it
+    for (int k = 0; k < data.Length; k++)
+        data[k] = rnd.Next(-100, 100);
+    outlet.PushSample(data);
+    Thread.Sleep(10);
+}
